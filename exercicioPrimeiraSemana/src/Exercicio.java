1fe23a6e34c70e364e636ec4c84d6f5463289ebe
Jayme Gomes <jayme.r.g.n@hotmail.com>
2020-08-09 21:35:39 -0300

    finalizacao do exericio

--- exercicioPrimeiraSemana/src/Exercicio.java
+++ exercicioPrimeiraSemana/src/Exercicio.java
@@ -3,6 +3,31 @@ import java.util.Random;
 
 public class Exercicio {
 
+
+    static double[][] matrizExDouble = new double[5][5];
+
+    static {
+        Random sorteioEx = new Random();
+        for (int i = 0; i < matrizExDouble.length; i++) {
+            for (int j = 0; j < matrizExDouble[0].length; j++) {
+                matrizExDouble[i][j] = sorteioEx.nextInt(100);
+            }
+
+        }
+    }
+
+    static int[][] matrizExInt = new int[5][5];
+
+    static {
+        Random sorteioEx = new Random();
+        for (int i = 0; i < matrizExInt.length; i++) {
+            for (int j = 0; j < matrizExInt[0].length; j++) {
+                matrizExInt[i][j] = sorteioEx.nextInt(100);
+            }
+
+        }
+    }
+
     static Scanner sc = new Scanner(System.in);
 
     // ------------- 1
@@ -103,7 +128,7 @@ public class Exercicio {
 
     // ------------- 4
 
-    public static int retornaQuant(Double[][] arrayBidimensional) {
+    public static int retornaQuant(double[][] arrayBidimensional) {
         return arrayBidimensional.length * arrayBidimensional[0].length;
     }
 
@@ -111,7 +136,7 @@ public class Exercicio {
 
     // ------------- 6
 
-    public static String retornaMatrizEmString(Double[][] arrayBidimensional) {
+    public static String retornaMatrizEmString(double[][] arrayBidimensional) {
         String saida = "";
 
         for (int i = 0; i < arrayBidimensional.length; i++) {
@@ -119,7 +144,7 @@ public class Exercicio {
                 if (j + 1 == arrayBidimensional[0].length)
                     saida += String.valueOf(arrayBidimensional[i][j]);
                 else
-                    saida += String.valueOf(arrayBidimensional[i][j]) + " - ";
+                    saida += arrayBidimensional[i][j] + " - ";
             }
             saida += "\n";
         }
@@ -129,7 +154,7 @@ public class Exercicio {
 
     // ------------- 7
 
-    public static double retornaMaior(Double[][] arrayBidimensional) {
+    public static double retornaMaior(double[][] arrayBidimensional) {
         double maior = 0;
 
         for (int i = 0; i < arrayBidimensional.length; i++) {
@@ -146,13 +171,238 @@ public class Exercicio {
         return maior;
     }
 
+    // ------------- 8
+
+    public static int retornaSoma(int[][] matizInt) {
+
+        int somaMatriz = 0;
+
+        for (int i = 0; i < matizInt.length; i++) {
+            for (int j = 0; j < matizInt[0].length; j++) {
+                somaMatriz += matizInt[i][j];
+            }
+        }
+
+        return somaMatriz;
+    }
+
+    // ------------- 9
+
+    public static double retornaMedia(double[][] arrayBidimensional) {
+
+        double soma = 0;
+
+        for (int i = 0; i < arrayBidimensional.length; i++) {
+            for (int j = 0; j < arrayBidimensional[0].length; j++) {
+                soma += arrayBidimensional[i][j];
+            }
+        }
+
+        return soma / (arrayBidimensional.length * arrayBidimensional[0].length);
+
+    }
+
+    // ------------- 10
+
+    public static void matrizChar() {
+
+        Random sorteio = new Random();
+        char[][] matrizChars = new char[sorteio.nextInt(5) + 1][sorteio.nextInt(3) + 1];
+        int contagem = 1;
+
+        for (int i = 0; i < matrizChars.length; i++) {
+            for (int j = 0; j < matrizChars[0].length; j++) {
+                System.out.print(contagem + "º char: ");
+                matrizChars[i][j] = sc.next().charAt(0);
+                contagem++;
+            }
+        }
+
+        for (int i = 0; i < matrizChars.length; i++) {
+            for (int j = 0; j < matrizChars[0].length; j++) {
+                System.out.print(matrizChars[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println(matrizChars.length + "...." + matrizChars[0].length);
+
+    }
+
+    // ------------- 11
+
+    public static void retornaPositivos(int[][] matrizInteiros) {
+
+        int contagemPositivos = 0;
+        int contagemArrayUni = 0;
+
+        for (int i = 0; i < matrizInteiros.length; i++) {
+            for (int j = 0; j < matrizInteiros[0].length; j++) {
+                if (matrizInteiros[i][j] > 0) {
+                    contagemPositivos++;
+                }
+            }
+        }
+
+        int[] arrayUniPositivo = new int[contagemPositivos];
+
+        for (int i = 0; i < matrizInteiros.length; i++) {
+            for (int j = 0; j < matrizInteiros[0].length; j++) {
+                if (matrizInteiros[i][j] > 0) {
+                    arrayUniPositivo[contagemArrayUni] = matrizInteiros[i][j];
+                    contagemArrayUni++;
+                }
+            }
+        }
+
+        for (int i = 0 ; i < contagemPositivos; i++){
+            System.out.print(arrayUniPositivo[i] + " ");
+        }
+
+    }
+
+    public static void menuEx1() {
+
+        int opcao = 30;
+
+        do {
+
+            System.out.println("---- Exercicio 1 ----");
+            System.out.println("ex a -- 1");
+            System.out.println("ex b -- 2");
+            System.out.println("ex c -- 3");
+            System.out.println("ex d -- 4");
+            System.out.println("sair -- 5");
+
+            System.out.print("Digite a opcao:... ");
+            opcao = sc.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    exercicio1a();
+                    break;
+
+                case 2:
+                    exercicio1b();
+                    break;
+
+                case 3:
+                    exercicio1c();
+                    break;
+
+                case 4:
+                    exercicio1d();
+                    break;
+
+                case 5:
+                    break;
+
+                default:
+                    System.err.println("selecione uma opcao correta");
+            }
+
+        } while (opcao != 5);
+
+
+    }
+
+    public static void menu() {
+
+        int opcao = 30;
+
+        do {
+
+            System.out.println("---- Menu do Exercicio ----");
+            System.out.println("ex 1");
+            System.out.println("ex 2");
+            System.out.println("ex 3");
+            System.out.println("ex 4");
+            System.out.println("ex 5");
+            System.out.println("ex 6");
+            System.out.println("ex 7");
+            System.out.println("ex 8");
+            System.out.println("ex 9");
+            System.out.println("ex 10");
+            System.out.println("ex 11");
+            System.out.println("12 PARA SAIR");
+            System.out.println();
+            System.out.print("Digite a opcao:... ");
+
+            opcao = sc.nextInt();
+
+            switch (opcao) {
+
+                case 1:
+                    menuEx1();
+                    System.out.println("--------------");
+                    break;
+
+                case 2:
+                    inteirosAleatorios();
+                    System.out.println("--------------");
+                    break;
+
+                case 3:
+                    matrizDouble();
+                    System.out.println("--------------");
+                    break;
+
+                case 4:
+                    System.out.println(retornaQuant(matrizExDouble));
+                    System.out.println("--------------");
+                    break;
+
+                case 5:
+                    System.out.println("digite outra opcao");
+                    System.out.println("--------------");
+                    break;
+
+                case 6:
+                    System.out.println(retornaMatrizEmString(matrizExDouble));
+                    System.out.println("--------------");
+                    break;
+
+                case 7:
+                    System.out.println(retornaMaior(matrizExDouble));
+                    System.out.println("--------------");
+                    break;
+
+                case 8:
+                    System.out.println(retornaSoma(matrizExInt));
+                    System.out.println("--------------");
+                    break;
+
+                case 9:
+                    System.out.println(retornaMedia(matrizExDouble));
+                    System.out.println("--------------");
+                    break;
+
+                case 10:
+                    matrizChar();
+                    System.out.println("--------------");
+                    break;
+
+                case 11:
+                    retornaPositivos(matrizExInt);
+                    System.out.println("--------------");
+                    break;
+
+                case 12:
+                    break;
+
+                default:
+                    System.out.println("escolha uma opcao disponivel");
+                    System.out.println("--------------");
+                    break;
+
+            }
+
+        } while (opcao != 12);
+    }
+
+    // ------------- 12
 
     public static void main(String[] args) {
-
-        Double[][] matrizDouble = new Double[2][5];
-
-        System.out.println(retornaMatrizEmString(matrizDouble));
-
-
+        menu();
     }
 }
