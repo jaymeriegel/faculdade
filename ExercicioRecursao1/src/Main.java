a7501068506a5eee5f3d399ef1c0bd35a3971f75
Jayme Gomes <jayme.r.g.n@hotmail.com>
2020-09-07 20:39:56 -0300

    finalizando mais um ex de Recursao

--- ExercicioRecursao1/src/Main.java
+++ ExercicioRecursao1/src/Main.java
@@ -67,53 +67,97 @@ public class Main {
         }
     }
 
-//    //Exercicio 7
-//    public static int encontraPosicaoMenorValor(int[] array, int tamanhoArray){
-//        if (tamanhoArray == 1){
-//            return tamanhoArray - 1;
-//        } else {
-//            if ((tamanhoArray == 2) && (array[tamanhoArray - 1] > array[tamanhoArray - 2])){
-//                return tamanhoArray - 1;
-//            } else
-//
-//        }
-//    } NAO CONSEGUI REALIZAR O EXERCICIO RECURSIVAMENTE.
+    public static int maximoR (int n, int v[]) {
+        int x;
+        if (n == 1) x = v[0];
+        else {
+            x = maximoR (n-1, v);
+            if (x < v[n-1]) x = v[n-1];
+        }
+        return x;
+    }
+
+    //Exercicio 7
+    public static int encontraPosicaoMenorValor(int[] array, int tamanhoVetor){
+
+        int numero;
+
+        if (tamanhoVetor == 0){
+            numero = tamanhoVetor;
+        } else {
+            numero = encontraPosicaoMenorValor(array, tamanhoVetor - 1);
+            if (array[numero] > array[tamanhoVetor - 1]) numero = tamanhoVetor - 1;
+        }
+
+        return numero;
+
+    }
 
     //Exercicio 8
-//    public static void printPrimaryDiagonal(double[][] matriz) throws MatrixNotPossibleException {
-//        if (matriz.length != matriz[0].length){
-//            throw new MatrixNotPossibleException();
-//        } else {
-//            if (matriz.length == 1){
-//                System.out.println(matriz[matriz.length - 1][matriz[0].length - 1]);
-//            } else {
-//
-//            }
-//        }
-//    }
-    //não consegui fazer recursivamente podendo haver somente 1 argumento.
+    private static void printPrimaryDiagonal(int[][] matriz, int tamanhoMatriz) throws MatrixNotPossibleException {
+        if (matriz.length != matriz[0].length){
+            throw new MatrixNotPossibleException();
+        } else {
+            if (tamanhoMatriz == 0){
+                System.out.println(matriz[tamanhoMatriz][tamanhoMatriz]);
+            } else {
+                System.out.println(matriz[tamanhoMatriz][tamanhoMatriz]);
+                printPrimaryDiagonal(matriz, tamanhoMatriz - 1);
+            }
+        }
+    }
+
+    public static void printPrimaryDiagonal(int[][] matriz){
+        try {
+            printPrimaryDiagonal(matriz, matriz.length - 1);
+        } catch (MatrixNotPossibleException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     //Exercicio 9
-//    public static int binaryToDecimal(String numeroBinario) {
-//        int aa = numeroBinario.charAt(0);
-//    }
 
+    private static int binaryToDecimal(String binarioString, int tamanhoString, int potenciaInicial){
+
+        if (tamanhoString == 1){
+            if (binarioString.charAt(0) == '0'){
+                return 0;
+            } else {
+                return (int) (Math.pow(2, potenciaInicial));
+            }
+
+        } else {
+
+            if (binarioString.charAt(tamanhoString - 1) == '0'){
+                return binaryToDecimal(binarioString, tamanhoString - 1, potenciaInicial + 1);
+            } else {
+                return (int) (Math.pow(2, potenciaInicial) + binaryToDecimal(binarioString, tamanhoString - 1, potenciaInicial + 1));
+            }
+
+        }
+    }
+
+    public static int binaryToDecimal(String binarioString){
+        return binaryToDecimal(binarioString, binarioString.length(), 0);
+    }
 
     public static void main(String[] args) {
-        double[][] vetorTeste = new double[2][2];
+        int[][] vetorTeste = new int[2][2];
+        int[] vetorTesteInt = new int[]{0, -1, 40, 5, -50};
 
         for (int i = 0; i < vetorTeste.length; i++) {
             for (int j = 0; j < vetorTeste[0].length; j++) {
-                vetorTeste[i][j] = 1;
+                vetorTeste[i][j] = 5;
             }
         }
+        printPrimaryDiagonal(vetorTeste);
 
-        for (int i = 0; i < vetorTeste.length; i++) {
-            for (int j = 0; j < vetorTeste[0].length; j++) {
-                System.out.println(vetorTeste[i][j]);
-            }
-        }
-        
+        char bi = '5';
 
+
+        String iii = "11111111";
+
+        System.out.println(binaryToDecimal(iii));
     }
 }
